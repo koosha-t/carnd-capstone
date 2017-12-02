3e6d558b6bc31e1f0c432e61dfbda012820de79c
Koosha <koosha.sbuces@gmail.com>
2017-12-02 12:48:07 -0800

    auto driving

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -10,30 +10,24 @@ from twist_controller import Controller
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
-
 You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
 You can subscribe to any other message that you find important or refer to the document for list
 of messages subscribed to by the reference implementation of this node.
-
 One thing to keep in mind while building this node and the `twist_controller` class is the status
 of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
-
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
 wheel_base) etc should not be altered in these files.
-
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
-
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
 that we have created in the `__init__` function.
-
 '''
 
 class DBWNode(object):
     def __init__(self):
-        rospy.init_node('dbw_node')
+        rospy.init_node('dbw_node', log_level=rospy.DEBUG)
 
         vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
         fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
@@ -54,11 +48,40 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `TwistController` object
-        # self.controller = TwistController(<Arguments you wish to provide>)
+	self.controller = Controller(wheel_base=wheel_base, steer_ratio=steer_ratio, min_speed=0.0, max_lat_accel= max_lat_accel, max_steer_angle=max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
 
-        self.loop()
+        self.current_velocity = TwistStamped()
+	self.target_velocity = TwistStamped()        
+
+	rospy.Subscriber('/current_velocity',TwistStamped,self.current_velocity_cb)
+        
+	rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb)
+        
+	self.dbw_enabled = False
+        rospy.Subscriber('/vehicle/dbw_enabled',Bool, self.dbw_enabled_cb)
+	
+	self.loop()
+
+    def current_velocity_cb(self, data):
+	self.current_velocity = data
+	rospy.loginfo("current_velocity cb linear: {0}, {1}, {2}".format(data.twist.linear.x, data.twist.linear.y, data.twist.linear.z))
+        rospy.loginfo("current_velocity cb angular: {0}, {1}, {2}".format(data.twist.angular.x, data.twist.angular.y, data.twist.angular.z))
+        pass
+
+
+    def twist_cmd_cb(self,data):
+	self.target_velocity = data
+	rospy.loginfo("twist cb linear: {0}, {1}, {2}".format(data.twist.linear.x, data.twist.linear.y, data.twist.linear.z))
+	rospy.loginfo("twist cb angular: {0}, {1}, {2}".format(data.twist.angular.x, data.twist.angular.y, data.twist.angular.z))
+	pass
+
+    def dbw_enabled_cb(self, data):
+	rospy.loginfo('dwb_enabled_cb:{}'.format(data.data))
+	self.dbw_enabled = data.data
+	pass
+
 
     def loop(self):
         rate = rospy.Rate(50) # 50Hz
@@ -70,8 +93,14 @@ class DBWNode(object):
             #                                                     <current linear velocity>,
             #                                                     <dbw status>,
             #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+	    
+            throttle, brake, steer = self.controller.control(current_velocity_linear=self.current_velocity.twist.linear, \
+		target_velocity_angular=self.target_velocity.twist.angular, target_velocity_linear=self.target_velocity.twist.linear, \
+		dbw_enabled=self.dbw_enabled) 
+	   
+            rospy.loginfo("current velocity cb linear: {0} {1} {2}".format(self.current_velocity.twist.linear.x, self.current_velocity.twist.linear.y, self.current_velocity.twist.linear.z))
+            if self.dbw_enabled:
+            	 self.publish(throttle, brake, steer)
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -95,3 +124,4 @@ class DBWNode(object):
 
 if __name__ == '__main__':
     DBWNode()
+
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,5 @@
+import rospy
+from yaw_controller import YawController
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -6,9 +8,39 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
+	# wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle
+	self.wheel_base = None
+	self.steer_ratio = None
+	self.min_speed = None
+	self.max_lat_accel = None
+	self.max_steer_angle = None
+	for key in kwargs:
+	    if key == 'wheel_base':
+	        self.wheel_base = kwargs[key]
+	    elif key == 'steer_ratio':
+		self.steer_ratio = kwargs[key]
+	    elif key == 'min_speed':
+		self.min_speed = kwargs[key]
+	    elif key == 'max_lat_accel':
+		self.max_lat_accel = kwargs[key]
+	    elif key == 'max_steer_angle':
+		self.max_steer_angle = kwargs[key]
+
+        #rospy.loginfo("args*:{}".format(args))
+        rospy.loginfo("args* kw:{}".format(kwargs))
+	
+	self.yaw_controller = YawController(self.wheel_base, self.steer_ratio,self. min_speed,self. max_lat_accel, self.max_steer_angle)
         pass
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+	if kwargs["dbw_enabled"] is False:
+	    return 0.,0.,0.
+	current_velocity_linear = kwargs["current_velocity_linear"]
+	target_velocity_linear = kwargs["target_velocity_linear"]
+	target_velocity_angular = kwargs["target_velocity_angular"]	
+	steer =self.yaw_controller.get_steering(target_velocity_linear.x, target_velocity_angular.z, current_velocity_linear.x)	
+        return 1., 0., steer
+
+
